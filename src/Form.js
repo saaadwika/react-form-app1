de536d39924e6452b06ae36eab5756ee2f08ee7b
saaadwika <saadwika.daparthi@sasi.ac.in>
2025-04-15 19:27:48 +0530

    Add basic Form component

---
+++ src/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Form() {
+  return (
+    <form>
+      <input type="text" placeholder="Name" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
